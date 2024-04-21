154c1b9fecdeee42f59b752fe206f5da91e86dae
Ethan Stelly <ethanlsbusiness@gmail.com>
2024-04-21 08:45:16 -0500

    Initial upload
    Contains Netbeans importable zip file along with the source files.

---
+++ MP4_Generics_Stelly/MP4_Generics_Stelly/src/main/java/com/mycompany/mp4_generics_stelly/MP4_Generics_Stelly.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mp4_generics_stelly;
+
+/**
+ *
+ * @author ethan
+ */
+public class MP4_Generics_Stelly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
